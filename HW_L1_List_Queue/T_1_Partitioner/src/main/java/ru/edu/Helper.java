c4100bdc64b37c8ac933fe2b0bc98546fa646134
Danchenko Viktor <VVDanchenko@sberbank.ru>
2022-09-04 16:17:47 +0300

    Changed method partitionBy Added test customTest

--- HW_L1_List_Queue/T_1_Partitioner/src/main/java/ru/edu/Helper.java
+++ HW_L1_List_Queue/T_1_Partitioner/src/main/java/ru/edu/Helper.java
@@ -1,5 +1,6 @@
 package ru.edu;
 
+import java.util.Objects;
 import java.util.function.Predicate;
 
 public class Helper {
@@ -19,8 +20,39 @@ public class Helper {
      * @param list - list
      * @param pred - predicate
      */
-
     public static <T> int partitionBy(Node<T> list, Predicate<T> pred) {
-        throw new RuntimeException("Not implemented");
+        int pos = 0;
+        if (Objects.isNull(list)) {
+            return pos;
+        }
+        Node<T> tmpNode = list;
+        while (list != null) {
+            tmpNode = list;
+            if (pred.test(list.value)) {
+                pos++;
+            } else {
+                if (list.next != null && pred.test(list.next.value)) pos++;
+                swap(tmpNode);
+                list = tmpNode;
+            }
+
+            list = list.next;
+        }
+
+        return pos;
+
+    }
+
+    private static <T> Node<T> swap(Node<T> tNode) {
+        Node<T> nextNode = null;
+        while (tNode != null) {
+            nextNode = tNode.next;
+            T notPredValue = tNode.value;
+            if (nextNode == null) break;
+            tNode.value = (T) nextNode.value;
+            nextNode.value = notPredValue;
+            tNode = tNode.next;
+        }
+        return tNode;
     }
 }
